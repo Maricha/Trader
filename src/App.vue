a0750950990f5dc91fb3c8c94359e6b0452026d4
Maricha <maricha177@gazeta.pl>
2017-04-07 11:27:42 +0200

    routes add

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <div id="app">
- 
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+        <div class="col-md-12">
+          <router-view></router-view>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Header from './components/Header.vue';
 
-export default {
-  name: 'app',
+  export default {
+    components: {
+      appHeader: Header
+    }
 
-}
+  }
 </script>
 
 <style>
-
+  body {
+    padding: 30px;
+  }
 </style>
